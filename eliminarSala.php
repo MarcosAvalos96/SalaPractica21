16afbd7b4fb6a88e3a9b2c2e652628e79c30bfee
jmanzanoa <javiermanzanoaguilar@ucol.mx>
2017-12-13 03:54:31 +0000

    issue de eliminar al 100

--- eliminarSala.php
+++ eliminarSala.php
@@ -7,7 +7,7 @@ if (isset($_GET["id"])) {
     $id = $_GET["id"];
 
 
-                $query = 'DELETE FROM SALA_REMOTA WHERE SALA_REMOTA='.$id;
+                $query = 'DELETE FROM SALA_REMOTA WHERE id_sala_remota='.$id;
                 mysql_query($query) or die(mysql_error());
 }
 
